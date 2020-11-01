395b313cb44a4359ece513a60215df376122e382
Dave Nicolson <david.nicolson@gmail.com>
2020-11-01 19:26:46 +0100

    Initial commit

---
+++ YouTubeRedux/AppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.h
+//  YouTubeRedux
+//
+//  Created by dave on 10/31/20.
+//
+//
+//
+//#import <UIKit/UIKit.h>
+//
+//@interface AppDelegate : UIResponder <UIApplicationDelegate>
+//
+//@property (strong, nonatomic) UIWindow *window;
+//
+//
+//@end
+
+
+#import <UIKit/UIKit.h>
+#import <TVMLKit/TVMLKit.h>
+
+@interface AppDelegate : UIResponder
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ YouTubeRedux/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  YouTubeRedux
+//
+//  Created by dave on 10/31/20.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, @"TVLApplication", @"TVLAppDelegate");
+    }
+}
